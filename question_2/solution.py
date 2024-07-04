96fe0f7bb33282325cbcc6bfca7c9942955753c2
Vladyslav Havriutkin <havriutkin@gmail.com>
2024-07-03 23:56:19 -0400

    Add solution description

--- question_2/solution.py
+++ question_2/solution.py
@@ -11,6 +11,18 @@ Assume an elevation map where the width of each bar is 1.
 
 Example: 
 Given [1,3,2,4,1,3,1,4,5,2,2,1,4,2,2], return 15 (3 bodies of water with volumes of 1,7,7 yields total volume of 15)
+
+Solution:
+    1. Create left[], where left[i] - the tallest hill on the left from i-th hill (left[0] = -1)
+    2. Create right[], where right[i] - the tallest hill on the right from i-th hill (right[N - 1] = -1)
+    3. Then, for i-th hill, the volume is min(left[i], right[i]) - height[i].
+        3.1 We ensure that volume is positive, i.e min(left[i], right[i]) > height[i]
+    4. Sum up all volumes
+
+Complexity:
+    1. O(n) - worst case time complexity
+    2. O(n) - space complexity, for left[], right[] and volumes[] each the size of n
+
 """
 from utils import utils
 
